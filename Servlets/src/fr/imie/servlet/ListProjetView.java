42678d2a6c597d678f31f6e752da7a3900d1b46e
ElenaKo <kobrinets@yahoo.com>
2013-09-04 15:11:05 +0200

    liste projets servlet et jsp ok

---
+++ Servlets/src/fr/imie/servlet/ListProjetView.java
@@ -0,0 +1,68 @@
+package fr.imie.servlet;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import fr.imie.formation.DAO.exceptions.DAOException;
+import fr.imie.formation.DTO.ProjetDTO;
+import fr.imie.formation.factory.DAOFactory1;
+import fr.imie.formation.services.interfaces.IProjetService;
+import fr.imie.formation.transactionalFramework.exception.TransactionalConnectionException;
+
+/**
+ * Servlet implementation class ListProjetView
+ */
+@WebServlet("/ListProjetView")
+public class ListProjetView extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ListProjetView() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		
+		if (request.getParameter("ligneProjet") == null) {
+		
+			IProjetService projetService = DAOFactory1.getInstance().createProjetService(null);
+			List<ProjetDTO> listeProjet= new ArrayList<ProjetDTO>();
+			try {
+				listeProjet=projetService.readAllProjets();
+				session.setAttribute("listeProjet", listeProjet);
+				
+				request.getRequestDispatcher("ListProjet.jsp")//remplace ancien redirect
+				.forward(request, response);
+			} catch (TransactionalConnectionException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (DAOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
